244f89ba99316a8ab287dbef650226b0c1225aa8
Administrador <Administrador@CAML192984>
2021-11-04 20:23:26 -0300

    MEDIA DE AVALIAÇÃO DO ALUNO

---
+++ MediaAluno1/src/mediaaluno1/MediaAluno1.java
@@ -0,0 +1,33 @@
+
+package mediaaluno1;
+
+import java.util.Scanner;
+
+public class MediaAluno1 {
+
+    
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner (System.in);
+        String aluno;
+        double nota1, nota2, nota3, media;
+        
+        System.out.println("Informe seu nome: ");
+        aluno = entrada.nextLine();
+        
+        System.out.println("Informe sua primeira nota: ");
+        nota1 = entrada.nextDouble();
+        
+        System.out.println("Informe sua segunda nota: ");
+        nota2 = entrada.nextFloat();
+        
+        System.out.println("Informe sua terceira nota: ");
+        nota3 = entrada.nextFloat();
+        
+        media = (nota1 + nota2 + nota3) / 3;
+        
+        System.out.println("A média do aluno(a) " +aluno + " nas avaliações é de: <"+media+">");
+        
+    }
+    
+}

cf7ceb8a2aaceaca46d1eb11317135ceb07c014e
Administrador <Administrador@CAML192984>
2021-11-03 21:31:08 -0300

    Mini calculadora com dois números mostrando o resultado; soma; subt; multi; divi

---
+++ MiniCalculadora/src/minicalculadora/MiniCalculadora.java
@@ -0,0 +1,44 @@
+
+package minicalculadora;
+
+import java.util.Scanner;
+
+
+public class MiniCalculadora {
+
+    
+    public static void main(String[] args) {
+      
+        //Declarando as variaveis
+        double num1, num2;
+        double soma, subt, multi, divi;
+        
+        //Biblioteca que irá permitir a interação do usuário
+        Scanner entrada = new Scanner(System.in);
+        
+        //Solicitar ao usuário os dois numeros
+           
+        System.out.println("--Mini Calculadora--");
+        
+        System.out.println("Digite um número:");
+        num1 = entrada.nextDouble();
+        System.out.println("Digite outro número:");
+        num2 = entrada.nextDouble();
+        
+        //Especificar o que cada variável faz
+        soma = num1 + num2;
+        subt = num1 - num2;
+        multi = num1 * num2;
+        divi = num1 / num2;        
+        
+        //Imprimir (mostrar) na tela os resultados
+        System.out.println("A soma dos números <" +num1 +"> e <" +num2 +"> é: " +soma);
+       System.out.println("A subtração dos números <" +num1 +"> e <" +num2 +"> é: " +subt);
+       System.out.println("A multiplicação dos números <" +num1 +"> e <" +num2 +"> é: " +multi);
+       System.out.println("A divisão dos números <" +num1 +"> e <" +num2 +"> é: " +divi);
+                
+        
+        
+    }
+    
+}
